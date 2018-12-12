be625d454e23a32e9695cb73df08080b85165ffd
weibiao <weibiao@hipad.com>
2018-12-12 16:39:16 +0800

    simple thread test

---
+++ simpleThreadTest/fuckcpp.cpp
@@ -0,0 +1,31 @@
+// thread example
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+ 
+void foo() 
+{
+  // do stuff...
+std::cout << "foo...\n";
+}
+
+void bar(int x)
+{
+  // do stuff...
+std::cout << "bar...\n";
+}
+
+int main() 
+{
+  std::thread first (foo);     // spawn new thread that calls foo()
+  std::thread second (bar,0);  // spawn new thread that calls bar(0)
+
+  std::cout << "main, foo and bar now execute concurrently...\n";
+
+  // synchronize threads:
+  first.join();                // pauses until first finishes
+  second.join();               // pauses until second finishes
+
+  std::cout << "foo and bar completed.\n";
+
+  return 0;
+}
